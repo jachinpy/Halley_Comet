0d98f82efad9fbf5b3da11277ae7603d4cbdf7c1
jachinpy <1689313450@qq.com>
2013-07-13 17:54:43 +0800

    test.py

--- HalleyComet/bit/tests.py
+++ HalleyComet/bit/tests.py
@@ -6,7 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+import unittest
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +14,22 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+#class TestCaseExample(unittest.TestCase):
+#	def setUp(self):
+#		print "call setUp"
+#		self.data = 100
+#	def tearDown(self):
+#		print "call tearDown"
+#		self.data = 101
+#	def testCase1(self):
+#		self.data = self.data+1
+#		self.assertEqual(self.data,101)
+#	def testCase2(self):
+#		self.data = self.data+1
+#		self.assert_(self.data > 100)
+#	def haha(self):
+#		print "call this?"
+#
+#if __name__ == '__main__':
+#				unittest.main()
