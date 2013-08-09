1ce1edd2e3d440ead376c065ee8bb7c0ef18405e
cpyou <745690939@qq.com>
2013-08-09 21:00:06 +0800

    优化了显示数据的方法

--- HalleyComet/bit/def_url.py
+++ HalleyComet/bit/def_url.py
@@ -2,6 +2,7 @@
 from bit.models import Url
 import hashlib
 import urlparse
+from django.utils import timezone
 
 def long_to_short(long_url):
     short_url = ''
@@ -15,6 +16,8 @@ def long_to_short(long_url):
         db_url = Url.objects.filter(long_url__exact=long_url)
         if db_url:
             short_url = db_url[0].short_url
+            db_url[0].visit_time = timezone.now()
+            db_url[0].save()
         else:
             short_url = hashlib.sha1(long_url).hexdigest()[:8]
             Url.objects.create(long_url=long_url, short_url=short_url)
