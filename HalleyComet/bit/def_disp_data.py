7a0f02ae9f47acf31d6fbc0fd5b47964644f95cf
cpyou <745690939@qq.com>
2013-08-11 02:01:07 +0800

    减少了显示数据数，修改了它的函数命名

--- HalleyComet/bit/def_disp_data.py
+++ HalleyComet/bit/def_disp_data.py
@@ -1,9 +1,7 @@
 from bit.models import Url
 
-def disp_data():
-    user_data = Url.objects.all().order_by('-visit_time')
-    if len(user_data) >= 10:
-        user_data = user_data[:10]
+def disp_five_data():
+    user_data = Url.objects.all().order_by('-visit_time')[:4]
     return user_data
 
 
