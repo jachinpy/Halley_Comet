c88c765a45cb0e9b765b547f1e5957be0c65b49a
cpyou <745690939@qq.com>
2013-08-11 02:14:35 +0800

    修改显示数

--- HalleyComet/bit/def_disp_data.py
+++ HalleyComet/bit/def_disp_data.py
@@ -1,7 +1,7 @@
 from bit.models import Url
 
 def disp_five_data():
-    user_data = Url.objects.all().order_by('-visit_time')[:4]
+    user_data = Url.objects.all().order_by('-visit_time')[:5]
     return user_data
 
 
